c99e7d75f51db0d628870a100527f3d3d2dbd0f2
Registree Rock <88082490+registree-org@users.noreply.github.com>
2022-10-18 22:48:41 +0200

    updated-slides

--- src/components/carousel-slide/slide.js
+++ src/components/carousel-slide/slide.js
@@ -19,12 +19,71 @@ function Slide() {
           src="../Assets/headerimage1.png"
           alt="First slide"
         />
-        <Carousel.Caption>
-          <div className="flex container align-middle" id="form-search">
-            
-            
-          </div>
-        </Carousel.Caption>
+       <Carousel.Caption>
+      <div className="container-fluid" id="form-search">
+      <div className="flex drop-form">
+      <DropdownButton
+      align="start"
+      title="Buy"
+      id="buy"
+      >
+      <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+      <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+      <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+      </DropdownButton>
+      </div>
+
+      <div className=" flex drop-form ">
+      <DropdownButton
+      align="start"
+      title="Residential"
+      id="residential"
+      >
+      <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+      <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+      <Dropdown.Item eventKey="3">Something else here</Dropdown.Item>
+      <Dropdown.Divider />
+      <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+      </DropdownButton>
+      </div>
+
+      <div className="flex  drop-form">
+      <input className="form-control" typeof='text-input' 
+      size={80} 
+      id="text-form" 
+      placeholder='Type Area or Surburb'></input>
+      </div>
+      
+      <Container>
+      <div className='container mt-5 pb-0' id='submit-button'>
+      <Button  className="btn-danger text-white" variant="contained">
+        SEARCH    
+      </Button>     
+      </div>
+
+      <div className='flex container mt-5 pb-0' >
+      <div className='col-4 mt-0 pb-0'>
+        <span id='listProperties'><b>124 Properties</b></span>
+      </div>
+      <DropdownButton   
+      align="end"
+      title="More Search Options "
+      id="buy2"
+      >       
+      <Dropdown.Item eventKey="6">
+     
+   
+      </Dropdown.Item> 
+      </DropdownButton>  
+      </div>
+      </Container> 
+</div>
+    
+    
+    
+    </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -78,13 +137,16 @@ function Slide() {
       </Button>     
       </div>
 
-      <div className='flex container mt-5 pb-0' id='submit-button'>
+      <div className='flex container mt-5 pb-0' >
+      <div className='col-4 mt-0 pb-0'>
+        <span id='listProperties'><b>124 Properties</b></span>
+      </div>
       <DropdownButton   
-      align="start"
+      align="end"
       title="More Search Options "
-      id="buy"
+      id="buy2"
       >       
-      <Dropdown.Item eventKey="2">
+      <Dropdown.Item eventKey="6">
      
    
       </Dropdown.Item> 
